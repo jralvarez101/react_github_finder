a13b4efa7a91509edc425a7e8e8cba9ef1f2265a
martin <alvarez.martinjr@gmail.com>
2021-07-06 21:20:16 -0700

    added alert to search bar

--- src/App.js
+++ src/App.js
@@ -1,40 +1,54 @@
-import React, { Component } from "react";
-import "./App.css";
-import Navbar from "./components/layout/Navbar";
-import Users from "./components/Users";
-import Search from './components/Search'
-import axios from "axios";
-import PropTypes from 'prop-types'
-
+import React, { Component } from 'react';
+import './App.css';
+import Navbar from './components/layout/Navbar';
+import Users from './components/Users';
+import Search from './components/Search';
+import axios from 'axios';
+import Alert from './components/layout/Alert';
 
 class App extends Component {
   state = {
-    users:[],
-    loading: false
-  }
+    users: [],
+    loading: false,
+    alert: null,
+  };
 
-  static propTypes = {
-    searchUsers: PropTypes.func.isRequired,
-  }
-
-  //This function was called inside the Search component and then passed up here as a prop where it 
+  //This function was called inside the Search component and then passed up here as a prop where it
   // takes the "text" state
-  searchUsers = async text => {
-    this.setState({loading: true})
+  searchUsers = async (text) => {
+    this.setState({ loading: true });
     // fetch the data
-    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
     // set state to false since it already fetched the data and set users to the new data
-    this.setState({users: res.data.items, loading: false})
-  }
-  
+    this.setState({ users: res.data.items, loading: false });
+  };
+
+  // clear users from state
+  clearUsers = () => this.setState({ users: [], loading: false });
+
+  // sets alert if nothing is entered in the search input field
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg: msg, type: type } });
+
+    setTimeout(() => this.setState({ alert: null }), 5000);
+  };
 
   render() {
+    const { users, loading } = this.state;
     return (
-      <div className="App">
-        <Navbar title="Github Finder" icon="fab fa-github" />
-        <div className="container">
-          <Search searchUsers={this.searchUsers}/>
-          <Users  loading={this.state.loading} users={this.state.users}/>
+      <div className='App'>
+        <Navbar title='Github Finder' icon='fab fa-github' />
+        <div className='container'>
+          <Alert alert={this.state.alert} />
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={users.length > 0 ? true : false}
+            setAlert={this.setAlert}
+          />
+          <Users loading={loading} users={users} />
         </div>
       </div>
     );
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,41 +1,64 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Search extends Component {
-    // input usually has a state attached to the input and you also must add an onchange event.
-    state = {
-        text:''
-    }
+  // input usually has a state attached to the input and you also must add an onchange event.
+  state = {
+    text: '',
+  };
 
-    // this onChange handler will work for all inputs because we are using e.target.name which is = to "text"
-    // if there was another name for an input it would still work because of this.
-    onChange = (e) => {
-        this.setState({[e.target.name]: e.target.value})
-    }
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired,
+    setAlert: PropTypes.func.isRequired,
+  };
 
-    onSubmit = (e) => {
-        e.preventDefault();
-        // In this case we will be passing the state in the form of props up to the main component (App.js)
-        this.props.searchUsers(this.state.text);
-        //reset state to nothing after it is submitted
-        this.setState({text: ''});
-    }
+  // this onChange handler will work for all inputs because we are using e.target.name which is = to "text"
+  // if there was another name for an input it would still work because of this.
+  onChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
 
+  onSubmit = (e) => {
+    e.preventDefault();
+    if (this.state.text === '') {
+      this.props.setAlert('Please enter something', 'light');
+    } else {
+      // In this case we will be passing the state in the form of props up to the main component (App.js)
+      this.props.searchUsers(this.state.text);
+      //reset state to nothing after it is submitted
+      this.setState({ text: '' });
+    }
+  };
 
   render() {
     return (
-      <form className="form" onSubmit={this.onSubmit}>
-        <input type="text" 
-        name="text" 
-        placeholder="Search users ..."  
-        value={this.state.text}
-        onChange={this.onChange}
-        />
-        <input
-          type="submit"
-          value="Search"
-          className="btn btn-dark btn-block"
-        />
-      </form>
+      <div>
+        <form className='form' onSubmit={this.onSubmit}>
+          <input
+            type='text'
+            name='text'
+            placeholder='Search users ...'
+            value={this.state.text}
+            onChange={this.onChange}
+          />
+          <input
+            type='submit'
+            value='Search'
+            className='btn btn-dark btn-block'
+          />
+        </form>
+        {this.props.showClear && (
+          <button
+            className='btn btn-light btn-block'
+            onClick={this.props.clearUsers}
+          >
+            {' '}
+            Clear
+          </button>
+        )}
+      </div>
     );
   }
 }
