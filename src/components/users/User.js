45af8cccd79412ef9a278f0acc055b48569c8241
martin <alvarez.martinjr@gmail.com>
2021-07-09 13:21:40 -0700

    profile card information display

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -33,6 +33,7 @@ class User extends Component {
       public_repos,
       public_gists,
       hireable,
+      company,
     } = this.props.user;
 
     const { loading } = this.props;
@@ -58,8 +59,16 @@ class User extends Component {
               alt=''
               style={{ width: "150px" }}
             />
-            <h1>{name || "User has no name in profile"}</h1>
-            <p>Location: {location || "User has no location"}</p>
+            {name && (
+              <Fragment>
+                <h1>{name}</h1>
+              </Fragment>
+            )}
+            {location && (
+              <Fragment>
+                <p>Location: {location}</p>
+              </Fragment>
+            )}
           </div>
           <div>
             {bio && (
@@ -71,7 +80,36 @@ class User extends Component {
             <a href={html_url} className='btn btn-dark my-1'>
               Visit Github Profile
             </a>
+            <ul>
+              <li>
+                {login && (
+                  <Fragment>
+                    <strong>Username:</strong> {login}
+                  </Fragment>
+                )}
+              </li>
+              <li>
+                {company && (
+                  <Fragment>
+                    <strong>Company:</strong> {company}
+                  </Fragment>
+                )}
+              </li>
+              <li>
+                {blog && (
+                  <Fragment>
+                    <strong>Website:</strong> {blog}
+                  </Fragment>
+                )}
+              </li>
+            </ul>
           </div>
+        </div>
+        <div className='card text-center'>
+          <div className='badge badge-primary'>Followers: {followers}</div>
+          <div className='badge badge-success'>Following: {following}</div>
+          <div className='badge badge-light'>Public Repos: {public_repos}</div>
+          <div className='badge badge-dark'>Public Gists: {public_gists}</div>
         </div>
       </Fragment>
     );
