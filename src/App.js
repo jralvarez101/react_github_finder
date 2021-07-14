494c2fe1b3871e9c3c7f0c79d384ca7895108c2a
martin <alvarez.martinjr@gmail.com>
2021-07-14 11:15:15 -0700

    Not Found page component created

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import AlertState from "./context/alert/AlertState";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Home from "./components/pages/Home";
+import NotFound from "./components/pages/NotFound";
 import User from "./components/users/User";
 import Alert from "./components/layout/Alert";
 import About from "./components/pages/About";
@@ -22,6 +23,7 @@ const App = () => {
                 <Route exact path="/" component={Home} />
                 <Route exact path="/about" component={About} />
                 <Route exact path="/user/:login" component={User} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
